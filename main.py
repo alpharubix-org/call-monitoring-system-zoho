77c25909e31244053a160ca589c8dcc8895ceae8
ashok <ashokmommyguy@gmail.com>
2025-05-25 22:46:31 +0530

    added requirements.txt file

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import getaccesstoken
 
 
 def main():
-    # access_token = getaccesstoken.get_access_token()
+    access_token = getaccesstoken.get_access_token()
     smtp = callmonitoringsystem.create_smtp_connection()
-    callmonitoringsystem.get_call_history(access_token='1000.cbb5f44f730a5e47cf51f8014500516a.d98af814c8a32c33570f5a4908c58f4f',smtp=smtp)
+    callmonitoringsystem.get_call_history(access_token=access_token,smtp=smtp)
     smtp.quit()
 
 
